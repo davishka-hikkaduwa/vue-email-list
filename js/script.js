89d5665c771121a4fca1a8f46395501ad35636e3
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-09-20 15:32:23 +0200

    completion

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 console.log('JS OK!');
 
+/*
+Descrizione:
+    Attraverso l'apposita API di Boolean
+    https://flynn.boolean.careers/exercises/api/random/mail
+    generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus:
+    Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
 const app = new Vue({
     el: '#app',
     data: {
-
+        emails: [],
     },
     methods: {
-
+        generateEmails() {
+            for (let i = 1; i <= 10; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        if (response.status === 200) {
+                            const randomEmail = response.data.response;
+                            this.emails.push(randomEmail);
+                        }
+                    });
+            }
+        },
     }
 })
